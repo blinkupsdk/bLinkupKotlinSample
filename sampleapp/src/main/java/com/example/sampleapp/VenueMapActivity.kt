7dbaf3743b6cd3f1cc61153f3981f73337c99352
unknown <chrismuto@gmail.com>
2023-11-01 14:02:24 -0700

    start of dropdown menu for map activity branch

--- sampleapp/src/main/java/com/example/sampleapp/VenueMapActivity.kt
+++ sampleapp/src/main/java/com/example/sampleapp/VenueMapActivity.kt
@@ -1,15 +1,26 @@
 package com.example.sampleapp
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.RecyclerView
 import com.blinkupapp.sdk.Blinkup
+import com.blinkupapp.sdk.data.exception.BlinkupException
+import com.blinkupapp.sdk.data.model.Place
+import com.blinkupapp.sdk.data.model.User
 import com.blinkupapp.sdk.view.VenueMapView
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class VenueMapActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var manager: RecyclerView.LayoutManager
+    private lateinit var myAdapter: RecyclerView.Adapter<*>
+    private lateinit var places: List<Place>
+    private lateinit var placeNames: List<String?>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_venue_map)
@@ -19,13 +30,19 @@ class VenueMapActivity : AppCompatActivity() {
 
         loading.visibility = View.VISIBLE
         lifecycleScope.launch(Dispatchers.IO) {
-            val places = Blinkup.getEvents()
+            places = Blinkup.getEvents()
+            placeNames = places.map{ it.name }
+            Log.i("placenames", "$placeNames")
             launch(Dispatchers.Main) {
                 map.place = places[0]
             }
             launch(Dispatchers.Main) {
                 loading.visibility = View.GONE
             }
+            launch(Dispatchers.Main) {
+                loading.visibility = View.GONE
+            }
+            myAdapter = VenueMapAdapter(placeNames)
         }
     }
 }
