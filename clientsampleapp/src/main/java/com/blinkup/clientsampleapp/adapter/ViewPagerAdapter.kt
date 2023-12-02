4367cd1f05d8be4824fa6e884194cacb06f064aa
wabowen89 <wabowen89@gmail.com>
2023-12-01 16:12:46 -0800

    added context to FragmentFriends

--- clientsampleapp/src/main/java/com/blinkup/clientsampleapp/adapter/ViewPagerAdapter.kt
+++ clientsampleapp/src/main/java/com/blinkup/clientsampleapp/adapter/ViewPagerAdapter.kt
@@ -1,5 +1,7 @@
 package com.blinkup.clientsampleapp.adapter
 
+import android.content.Context
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
@@ -8,14 +10,14 @@ import com.blinkup.clientsampleapp.tabs.FragmentMap
 import com.blinkup.clientsampleapp.tabs.FragmentPresence
 import com.blinkup.clientsampleapp.tabs.FragmentSettings
 
-class ViewPagerAdapter(fragmentActivity: FragmentActivity) :
+class ViewPagerAdapter(fragmentActivity: FragmentActivity, var context: Context) :
     FragmentStateAdapter(fragmentActivity) {
 
     override fun getItemCount(): Int = 4
 
     override fun createFragment(position: Int): Fragment {
         return when (position) {
-            0 -> FragmentFriends()
+            0 -> FragmentFriends(context)
             1 -> FragmentPresence()
             2 -> FragmentMap()
             3 -> FragmentSettings()
